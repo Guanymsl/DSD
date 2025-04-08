b391d14c5b73853b02af2bd00c77da2570b8086a
Guanymsl <chadlee9402@gmail.com>
2025-04-08 16:07:54 +0800

    Simplify Comb

--- DSD_HW2/verilog/CHIP_syn.tcl
+++ DSD_HW2/verilog/CHIP_syn.tcl
@@ -10,27 +10,27 @@ read_verilog memory.v
 set DESIGN "CHIP"
 current_design [get_designs $DESIGN]
 
-#You may modified the clock constraints 
+#You may modified the clock constraints
 #or add more constraints for your design
 ####################################################
-set cycle  10
+set cycle  3.5
 ####################################################
 
 #The following are design spec. for synthesis
-#You can NOT modify this seciton 
+#You can NOT modify this seciton
 #####################################################
 create_clock -name CLK -period $cycle [get_ports clk]
 set_fix_hold                          [get_clocks CLK]
 set_dont_touch_network                [get_clocks CLK]
 set_ideal_network                     [get_ports clk]
-set_clock_uncertainty            0.1  [get_clocks CLK] 
-set_clock_latency                0.5  [get_clocks CLK] 
+set_clock_uncertainty            0.1  [get_clocks CLK]
+set_clock_latency                0.5  [get_clocks CLK]
 
-set_max_fanout 6 [all_inputs] 
+set_max_fanout 6 [all_inputs]
 
 
 set_operating_conditions -min_library fast -min fast -max_library slow -max slow
-set_wire_load_model -name tsmc13_wl10 -library slow  
+set_wire_load_model -name tsmc13_wl10 -library slow
 set_drive        1     [all_inputs]
 # set_load         1     [all_outputs]
 set t_in  0.1
@@ -44,7 +44,7 @@ set_input_delay  $t_in  -clock CLK [get_ports rst_n]
 
 
 #Compile and save files
-#You may modified setting of compile 
+#You may modified setting of compile
 
 compile_ultra -no_autoungroup
 #####################################################
@@ -80,7 +80,7 @@ write_sdc   -version 1.8                ./Netlist/${DESIGN}_syn.sdc
 # when simulation, we read the original memory.v as the memory module inference
 remove_design memory
 write   -f verilog  -hierarchy  -output ./Netlist/${DESIGN}_syn.v
-#####################################################  
+#####################################################
 
 # restore the memory design for further improvement if needed (remember to remove the memory design before writing verilog)
 read_ddc ./Netlist/${DESIGN}_syn.ddc
--- DSD_HW2/verilog/core.v
+++ DSD_HW2/verilog/core.v
@@ -29,7 +29,6 @@ module core(clk,
     reg  [31:0] rd_data;
 
     reg [31:0] instruction;
-    reg [31:0] alu_result;
 
     reg        wen_D_reg;
     reg [31:0] addr_D_reg;
@@ -52,15 +51,15 @@ module core(clk,
         .q2(rs2_data)
     );
 
-    assign mem_wen_D   = wen_D_reg;
-    assign mem_addr_D  = addr_D_reg;
+    assign mem_wen_D  = wen_D_reg;
+    assign mem_addr_D = addr_D_reg;
+    assign mem_addr_I = PC;
+
     assign {mem_wdata_D[7:0], mem_wdata_D[15:8], mem_wdata_D[23:16], mem_wdata_D[31:24]} = wdata_D_reg[31:0];
-    assign mem_addr_I  = PC;
 
     always @(*) begin
         instruction[31:0] = {mem_rdata_I[7:0], mem_rdata_I[15:8], mem_rdata_I[23:16], mem_rdata_I[31:24]};
 
-        alu_result  = 0;
         imm         = 0;
         addr_D_reg  = 0;
         wdata_D_reg = 0;
@@ -74,50 +73,38 @@ module core(clk,
         rs2    = instruction[24:20];
         rd     = instruction[11:7];
         funct3 = instruction[14:12];
-        funct7 = instruction[31:25];
+        funct7 = instruction[30];
 
         case (opcode)
             // R-type instructions: add, sub, and, or
             7'b0110011: begin
                 case (funct7)
-                    7'b0000000: rd_data = (funct3 == 000) ? rs1_data + rs2_data :    // ADD
-                                          (funct3 == 111) ? rs1_data & rs2_data :    // AND
-                                          (funct3 == 110) ? rs1_data | rs2_data : 0; // OR
-                    7'b0100000: rd_data = (funct3 == 000) ? rs1_data - rs2_data : 0; // SUB
+                    1'b0: rd_data = (funct3 == 3'b000) ? rs1_data + rs2_data :          // ADD
+                                          (funct3 == 3'b111) ? rs1_data & rs2_data :    // AND
+                                          (funct3 == 3'b110) ? rs1_data | rs2_data : 0; // OR
+                    1'b1: rd_data = rs1_data - rs2_data;                                // SUB
                     default: rd_data = 0;
                 endcase
                 regWrite = 1;
             end
 
-            // I-type instructions: slt
+            // R-type instructions: slt
             7'b0010011: begin
-                imm = {{20{instruction[31]}}, instruction[31:20]};
-                case (funct3)
-                    3'b010: rd_data = ($signed(rs1_data) < $signed(imm)) ? 1 : 0; // SLTI
-                    default: rd_data = 0;
-                endcase
+                rd_data = ($signed(rs1_data) < $signed(rs2_data)) ? 1 : 0; // SLT
                 regWrite = 1;
             end
 
             // I-type instructions: lw
             7'b0000011: begin
-                imm = {{20{instruction[31]}}, instruction[31:20]};
-                case (funct3)
-                    3'b010: alu_result = rs1_data + imm; // LW
-                    default: alu_result = 0;
-                endcase
-                addr_D_reg    = alu_result;
+                imm           = {{20{instruction[31]}}, instruction[31:20]};
+                addr_D_reg    = rs1_data + imm;
                 rd_data[31:0] = {mem_rdata_D[7:0], mem_rdata_D[15:8], mem_rdata_D[23:16], mem_rdata_D[31:24]};
                 regWrite      = 1;
             end
 
             // B-type instructions: beq
             7'b1100011: begin
-                case (funct3)
-                    3'b000: alu_result = (rs1_data == rs2_data) ? 1 : 0; // BEQ
-                    default: alu_result = 0;
-                endcase
-                if (alu_result) begin
+                if (rs1_data == rs2_data) begin
                     imm = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
                     PC_nxt = PC + imm;
                 end
@@ -125,43 +112,33 @@ module core(clk,
 
             // S-type instructions: sw
             7'b0100011: begin
-                imm = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
-                case (funct3)
-                    3'b010: alu_result = rs1_data + imm; // SW
-                    default: alu_result = 0;
-                endcase
-                addr_D_reg  = alu_result;
+                imm         = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+                addr_D_reg  = rs1_data + imm;
                 wdata_D_reg = rs2_data;
                 wen_D_reg   = 1;
             end
 
             // J-type instructions: jal
             7'b1101111: begin
-                imm = {{20{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
-                alu_result = PC + imm; // JAL
-                PC_nxt     = alu_result;
-                rd_data    = PC + 4;
-                regWrite   = 1;
-            end
-
-            // I-type instructions: jalr
-            7'b1100111: begin
-                imm = {{20{instruction[31]}}, instruction[31:20]};
-                case (funct3)
-                    3'b000: alu_result = (rs1_data + imm) & ~1; // JALR
-                    default: alu_result = 0;
-                endcase
-                PC_nxt   = alu_result;
+                imm      = {{20{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+                PC_nxt   = PC + imm;
                 rd_data  = PC + 4;
                 regWrite = 1;
             end
 
+            // I-type instructions: jalr
+            7'b1100111: begin
+                imm      = {{20{instruction[31]}}, instruction[31:20]};
+                PC_nxt   = rs1_data + imm;
+                rd_data  = PC + 4;
+                regWrite = 1;
+            end
         endcase
     end
 
     always @(posedge clk) begin
         if (!rst_n) begin
-            PC <= 32'h00000000;
+            PC <= 32'd0;
         end
         else begin
             PC <= PC_nxt;
@@ -176,8 +153,7 @@ module reg_file(clk, rst_n, wen, a1, a2, aw, d, q1, q2);
 
     output [31:0] q1, q2;
 
-    reg [31:0] mem     [0:31];
-    reg [31:0] mem_nxt [0:31];
+    reg [31:0] mem [0:31];
 
     assign q1 = mem[a1];
     assign q2 = mem[a2];
