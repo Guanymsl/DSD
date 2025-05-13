77b7e6fb0527cee712b4bd7de7af9e99d61dabee
Guanymsl <chadlee9402@gmail.com>
2025-05-14 02:48:23 +0800

    Fix Syntax

--- DSD_HW3/cache_dm.v
+++ DSD_HW3/cache_dm.v
@@ -16,20 +16,20 @@ module cache(
 );
 
 //==== input/output definition ============================
-    input          clk;
+    input        clk;
     // processor interface
-    input          proc_reset;
-    input          proc_read, proc_write;
-    input   [29:0] proc_addr;
-    input   [31:0] proc_wdata;
-    output         proc_stall;
-    output  [31:0] proc_rdata;
+    input        proc_reset;
+    input        proc_read, proc_write;
+    input [29:0] proc_addr;
+    input [31:0] proc_wdata;
+    output            proc_stall;
+    output reg [31:0] proc_rdata;
     // memory interface
-    input  [127:0] mem_rdata;
-    input          mem_ready;
-    output         mem_read, mem_write;
-    output  [27:0] mem_addr;
-    output [127:0] mem_wdata;
+    input [127:0] mem_rdata;
+    input         mem_ready;
+    output reg         mem_read, mem_write;
+    output reg [ 27:0] mem_addr;
+    output reg [127:0] mem_wdata;
 
 //==== wire/reg definition ================================
 localparam S_IDLE   = 2'd0;
@@ -44,14 +44,16 @@ reg [1:0] state_r, state_w;
 reg [154:0] cache_r [0:7];
 reg [154:0] cache_w [0:7];
 
-reg req_read_r, req_write_r;
+wire [24:0] req_tag;
+wire [ 2:0] req_index;
+wire [ 1:0] req_offset;
 
-wire [23:0] req_tag    = proc_addr[29:5];
-wire [2:0]  req_index  = proc_addr[4:2];
-wire [3:0]  req_offset = proc_addr[1:0];
+assign req_tag    = proc_addr[29:5];
+assign req_index  = proc_addr[4:2];
+assign req_offset = proc_addr[1:0];
 
 wire valid, dirty, hit;
-wire [23:0] tag;
+wire [24:0] tag;
 
 assign valid = cache_r[req_index][154];
 assign dirty = cache_r[req_index][153];
@@ -59,8 +61,6 @@ assign tag   = cache_r[req_index][152:128];
 
 assign hit        = (valid && tag == req_tag);
 assign proc_stall = (state_r != S_IDLE);
-
-reg [127:0] evict_data;
 
 integer i;
 
@@ -96,13 +96,11 @@ always @(*) begin
     case (state_r) // synopsys parallel_case full_case
         S_LOOKUP: begin
             if (hit) begin
-                if (req_read_r) begin
-                    proc_rdata = cache_r[req_index][(req_offset << 5)+:32];
+                if (proc_read) begin
+                    proc_rdata = cache_r[req_index][(req_offset * 32)+:32];
                 end
-                if (req_write_r) begin
-                    // update word and set dirty
-                    cache_w[req_index][127:0] &= (32'd0 << (req_offset << 5));
-                    cache_w[req_index][127:0] |= (proc_wdata << (req_offset << 5));
+                if (proc_write) begin
+                    cache_w[req_index][(req_offset * 32)+:32] = proc_wdata;
                     cache_w[req_index][153] = 1'b1;
                     proc_rdata = proc_wdata;
                 end
@@ -110,21 +108,20 @@ always @(*) begin
         end
         S_WB: begin
             mem_write = 1'b1;
-            mem_addr  = req_addr_r[29:4];
+            mem_addr  = {cache_r[req_index][152:128], req_index};
             mem_wdata = cache_r[req_index][127:0];
         end
         S_REFILL: begin
             mem_read = 1'b1;
-            mem_addr = req_addr_r[29:4];
+            mem_addr = req_tag;
             if (mem_ready) begin
                 cache_w[req_index] = {1'b1, 1'b0, req_tag, mem_rdata};
-                if (req_write_r) begin
-                    cache_w[req_index][127:0] &= (32'd0 << (req_offset << 5));
-                    cache_w[req_index][127:0] |= (proc_wdata << (req_offset << 5));
+                if (proc_write) begin
+                    cache_w[req_index][(req_offset * 32)+:32] = proc_wdata;
                     cache_w[req_index][153] = 1'b1;
                     proc_rdata = proc_wdata;
                 end else begin
-                    proc_rdata = cache_w[req_index][(req_offset << 5)+:32];
+                    proc_rdata = cache_w[req_index][(req_offset * 32)+:32];
                 end
             end
         end
@@ -134,15 +131,11 @@ end
 //==== sequential circuit =================================
 always @(posedge clk) begin
     if (proc_reset) begin
-        state_r      <= S_IDLE;
-        req_read_r   <= 1'b0;
-        req_write_r  <= 1'b0;
+        state_r <= S_IDLE;
         for (i = 0; i < 8; i = i + 1)
             cache_r[i] <= 155'd0;
     end else begin
-        state_r      <= state_w;
-        req_read_r   <= proc_read;
-        req_write_r  <= proc_write;
+        state_r <= state_w;
         for (i = 0; i < 8; i = i + 1)
             cache_r[i] <= cache_w[i];
     end
